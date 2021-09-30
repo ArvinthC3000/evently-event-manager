c107450d4362b4f6a84bfbc3c13153260a839710
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-09-30 09:24:09 +0530

    userReducer

---
+++ src/reducers/userReducer.jsx
@@ -0,0 +1,17 @@
+const initialState = {
+  user: null,
+  current: null,
+  loading: false,
+  error: null,
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case '':
+      break;
+
+    default:
+      break;
+  }
+};
