1c0083576f3df1d593d2762dcc6928789efddf87
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-10-03 21:30:30 +0530

    BE URI change

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,3 +1,4 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 
-export const URI = 'http://localhost:5000';
+// export const URI = 'http://localhost:5000';
+export const URI = 'https://murmuring-eyrie-98867.herokuapp.com';
