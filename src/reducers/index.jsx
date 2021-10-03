fcfe39125c703c8664f59df24e9a757c16f2fc25
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-10-03 13:55:30 +0530

    Updates

--- src/reducers/index.jsx
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import eventReducer from './eventReducer';
-import userReducer from './userReducer';
-
-export default combineReducers({
-  user: userReducer,
-  events: eventReducer,
-});
